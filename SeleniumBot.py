3a70c45eb91d4aedddea300d902eaa40734833fd
GuilhermeTravaglia <guilhermeetll@gamil.com>
2022-08-20 10:29:00 -0300

    Pequenos ajustes

--- SeleniumBot.py
+++ SeleniumBot.py
@@ -1,5 +1,3 @@
-from distutils.spawn import find_executable
-from xml.dom.minidom import Element
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -23,23 +21,26 @@ def login_quotex():
     navegador.find_element(By.XPATH, campo_email_login).send_keys(email)   # preenche campo email
     navegador.find_element(By.XPATH, campo_senha_login).send_keys(senha)   # preenche campo senha
     navegador.find_element(By.XPATH, campo_login_botao).click()            # clica no botao de 'login'
+    print('\nNavegador carregado')
     return navegador
 
 
 #Muda para o modo de Demo, tirando da conta real
-def muda_conta_demo(navegador):
-    try:
+def muda_para_conta_demo(navegador):
+    try: #Espera pagina carregar
         WebDriverWait(navegador, 30).until(EC.presence_of_element_located((By.XPATH, espera_broker_carregar)))
-    except:
-        muda_conta_demo(navegador)
-    finally:
+    except: #Caso nao carregue em 30s
+        muda_para_conta_demo(navegador)
+    finally: #Quando carrega muda a conta
         navegador.find_element(By.XPATH, menu_conta_demo).click()
         navegador.find_element(By.XPATH, botao_conta_demo).click()
+        print('Conta demo ativada')
 
 
 #Navega ate a sala de sinais
 def clica_sala_de_sinais(navegador):
     navegador.find_element(By.XPATH, sala_de_sinais).click()
+    print('Sala de sinais ativada')
 
 
 #Clica no ativo que apareceu o sinal
@@ -49,36 +50,36 @@ def primeiro_sinal(navegador):
     finally:
         navegador.find_element(By.XPATH, seleciona_sinal).click()
     sleep(0.2)
+    print('Primeira negociacao lida')
     return ativo
 
 
 #Roda o Robo
-def loop_execucao(navegador, ativo):
+def le_sinal_e_executa(navegador, ativo):
+    print('Operando')
     ativoProx = ativo
     num_operacoes = 0
-    while (num_operacoes < num_max_operacoes):
+    while (num_operacoes <= num_max_operacoes):
         try:
             WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.XPATH, seleciona_sinal)))
             ativo = navegador.find_element(By.XPATH, seleciona_sinal).text
-            if ativoProx != ativo:
-                ativoProx = ativo
-                navegador.find_element(By.XPATH, seleciona_sinal).click()
-                try:
+            if ativoProx != ativo: #Compara se o ativo mudou
+                ativoProx = ativo  #Atribui o novo ativo para o proximo
+                navegador.find_element(By.XPATH, seleciona_sinal).click() #Seleciona esse novo ativo
+                try: #Verifica se o sinal foi de compra
                     navegador.find_element(By.XPATH, le_sinal_compra)
-                    WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.XPATH, botao_compra)))
                     navegador.find_element(By.XPATH, botao_compra).click()
-                except:
-                    WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.XPATH, botao_venda)))
-                    navegador.find_element(By.XPATH, botao_venda).click()
+                except: #Se o sinal de compra não for encontrado retornará um ERRO fazendo assim o sinal de venda executar
+                    navegador.find_element(By.XPATH, botao_venda).click() #Sinal de venda
                 num_operacoes += 1
         except:
-            sleep(0.5)
+            pass
 
 def main():
     navegador = login_quotex()
-    muda_conta_demo(navegador)
+    muda_para_conta_demo(navegador)
     clica_sala_de_sinais(navegador)
     ativo = primeiro_sinal(navegador)
-    loop_execucao(navegador, ativo)
+    le_sinal_e_executa(navegador, ativo)
     
 main()
